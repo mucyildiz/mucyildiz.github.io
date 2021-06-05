5c93e68c182ec2eb10fff7757af6d637d77e2efa
Alperen Yildiz <alperenyildiz21@gmail.com>
2021-06-04 22:18:36 -0400

    added greeting part

---
+++ client/src/components/Introduction.js
@@ -0,0 +1,13 @@
+import './Introduction.css';
+
+function Introduction() {
+  return (
+    <div id='intro-container'>
+      <div id='greeting'>Hello! My name is</div>
+      <div id='name'>Mucahit Alperen Yildiz.</div>
+      <div id='subtitle'>I enjoy making things.</div>
+    </div>
+  )
+}
+
+export default Introduction;
