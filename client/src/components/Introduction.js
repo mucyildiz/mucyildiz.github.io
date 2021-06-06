e899c0354716f044c120b6b6cc25973c335ed6d5
Alperen Yildiz <alperenyildiz21@gmail.com>
2021-06-05 23:58:07 -0400

    Added contact me button for emailing

--- client/src/components/Introduction.js
+++ client/src/components/Introduction.js
@@ -4,8 +4,22 @@ function Introduction() {
   return (
     <div id='intro-container'>
       <div id='greeting'>Hello! My name is</div>
-      <div id='name'>Mucahit Alperen Yildiz.</div>
-      <div id='subtitle'>I enjoy making things.</div>
+      <div id='name'>Mucahit Alperen Yildiz.
+        <br/> 
+        <span id='subtitle'>I enjoy being creative.</span>
+      </div>
+      <div id='message'>
+        I am an aspiring software developer fresh out of the
+        University of Massachusetts Amherst. My degree in Mathematics
+        with concentrations in Data Science and Computing taught me 
+        both coding and critical thinking skills that I utilize daily in creating
+        web applications.
+      </div>
+      <a href='mailto:alperenyildiz21@gmail.com'>
+        <button id='contact'>
+          Contact Me!
+        </button>
+      </a>
     </div>
   )
 }
